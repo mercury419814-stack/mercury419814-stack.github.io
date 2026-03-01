d37c9f90cecc31748590a613cd5dd5fc096586ab
mercury419814-stack <mercury419814-stack@users.noreply.github.com>
2026-03-01 14:06:14 +0000

    deploy: 2b0b49c77f9034454ea72068cd2ebb62a58c866d

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1772358491',resources: [
+    cacheName: 'chirpy-1772373973',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
