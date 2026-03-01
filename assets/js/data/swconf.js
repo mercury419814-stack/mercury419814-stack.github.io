e03a9269c1e778f44ef2201d988160ecfe93c514
mercury419814-stack <mercury419814-stack@users.noreply.github.com>
2026-03-01 09:39:48 +0000

    deploy: 6bec6c108f7a8014084b1a48df4ccc02ddc6dc13

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1772357987',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+        '/friends/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://hjinkuo.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
